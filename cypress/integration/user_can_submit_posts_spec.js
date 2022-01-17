a2d59ed71037a1c015f72a57d89d8c58e127e9f7
Anamaria-Mocanu <Annamaariaaa@yahoo.com>
2022-01-17 14:59:52 +0000

    fixed the tests, all running now

--- cypress/integration/user_can_submit_posts_spec.js
+++ cypress/integration/user_can_submit_posts_spec.js
@@ -1,11 +1,22 @@
 describe('Timeline', function() {
   it('can submit posts and view them', function() {
+    cy.visit('/users/new');
+    cy.get('#new-user-form').find('[type="email"]').type('123@hotmail.com');
+    cy.get('#new-user-form').find('[type="password"]').type('1234');
+    cy.get('#new-user-form').submit();
+
+    cy.visit('/sessions/new');
+    cy.get('#new-session-form').find('[type="email"]').type('123@hotmail.com');
+    cy.get('#new-session-form').find('[type="password"]').type('1234');
+    cy.get('#new-session-form').submit();
+
+    
     cy.visit('/posts');
     cy.contains('New post').click();
 
     cy.get('#new-post-form').find('[type="text"]').type('Hello, world!');
     cy.get('#new-post-form').submit();
-
+    cy.visit('/posts');
     cy.get('.posts').should('contain', 'Hello, world!');
   });
 });
--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
+// eslint-disable-next-line node/no-missing-require
 var UsersController = require('../controllers/Users');
 
 router.get('/new', UsersController.New);

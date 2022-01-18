27ab099b5311ae823e08a1c4450477eacc0348df
Anamaria-Mocanu <Annamaariaaa@yahoo.com>
2022-01-18 16:10:14 +0000

    added stuff in the webpack config file

---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+// var webpack = require("webpack");
+var path = require('path')
+module.exports = {
+  devServer: {
+  },
+  devtool: 'source-map',
+  entry: {
+    "ym.min": "./src/bootstrap.ts",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        enforce: "pre",
+        use: [
+          {
+            loader: "tslint-loader"
+          }
+        ]
+      },
+      {
+        test: /\.ts$/,
+        use: [
+          {
+            loader: "ts-loader"
+          }
+        ]
+      }
+    ]
+  }, // End of module
+  output: {
+    filename: '[name].js',
+    path: './dist'
+  },
+  plugins: [],
+  resolve: {
+    modules: [
+      "node_modules",
+      path.join(__dirname, 'node_modules')
+    ],
+    extensions: [".webpack.js", ".web.js", ".ts", ".js", "*"],
+    enforceExtension: false,
+    alias: {
+      'handlebars': 'handlebars/runtime.js'
+    }
+  }
+}

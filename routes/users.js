46c3c8c1d609953aaf2573cbffa377d6f940b801
ClaimingCookie5 <hotunz@gmail.com>
2022-01-18 17:00:55 +0000

    HE - Fixxing path to controllers/users

--- routes/users.js
+++ routes/users.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 // eslint-disable-next-line node/no-missing-require
-var UsersController = require('../controllers/Users');
+var UsersController = require('../controllers/users');
 
 router.get('/new', UsersController.New);
 router.post('/', UsersController.Create);

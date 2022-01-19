55735b2fbbd8be9774ae3e785a9b5966c8d09e0e
Jessica Bulman <jessicabulman97@gmail.com>
2022-01-19 14:45:13 +0000

    adding npm start

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
   apps: [
     {
       name: 'aws-codedeploy',
-      script: '/bin/www',
+      script: 'npm start',
       args: 'serve -s build -l 3000 -n',
       interpreter: 'none',
       env_production: {

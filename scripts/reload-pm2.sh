2067a532d6e45b0583090ff75fc8796d8bbedb02
Jessica Bulman <jessicabulman97@gmail.com>
2022-01-19 16:30:51 +0000

    changed reload-pm2.sh

--- scripts/reload-pm2.sh
+++ scripts/reload-pm2.sh
@@ -2,5 +2,8 @@
 pwd
 cd /home/ec2-user/acebook-t1w3
 pwd
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
 npm install pm2 -g
 pm2 --name acebook-t1w3 start npm -- start

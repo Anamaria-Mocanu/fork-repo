752d93f5f694874f80212223c282cb43cbcabb83
ClaimingCookie5 <hotunz@gmail.com>
2022-01-20 11:56:00 +0000

    HE - Changing script

--- scripts/reload-pm2.sh
+++ scripts/reload-pm2.sh
@@ -4,5 +4,6 @@ cd /home/ec2-user/acebook-t1w3
 # nvm bash_completion
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+npm install
 npm i pm2@latest -g
 pm2 start npm --name "acebook-t1w3" -- start

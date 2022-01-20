c026fa7dfff36f56ea937b93799f7b7c1eb594d0
ClaimingCookie5 <hotunz@gmail.com>
2022-01-20 11:43:41 +0000

    HE - Adding stuff

--- scripts/reload-pm2.sh
+++ scripts/reload-pm2.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 echo "Changing directory to acebook-t1w3"
 cd /home/ec2-user/acebook-t1w3
+# nvm bash_completion
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 sudo npm i pm2@latest -g
 pm2 start npm --name "acebook-t1w3" -- start

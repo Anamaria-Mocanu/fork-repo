6cc381d962c1866c5b603d0ef4e395ed57efa0de
Anamaria-Mocanu <Annamaariaaa@yahoo.com>
2022-01-20 15:21:11 +0000

    commented out the mongodb steps and getting the nvm

--- scripts/reload-pm2.sh
+++ scripts/reload-pm2.sh
@@ -2,28 +2,28 @@
 echo "Changing directory to acebook-t1w3"
 cd /home/ec2-user/acebook-t1w3
 
-# Geting the nvm
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
-. ~/.nvm/nvm.sh
+# # Geting the nvm
+# curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
+# . ~/.nvm/nvm.sh
 # nvm bash_completion
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 nvm install 12.14.1
 
-# Installing mongodb
-cd /etc/yum.repos.d/
-touch mongodb-org-4.0.repo
-echo "[mongodb-org-4.0]
-name=MongoDB Repository
-baseurl=https://repo.mongodb.org/yum/amazon/2013.03/mongodb-org/4.0/x86_64/
-gpgcheck=1
-enabled=1
-gpgkey=https://www.mongodb.org/static/pgp/server-4.0.asc" >> mongodb-org-4.0.repo
-yum install -y mongodb-org
-systemctl start mongod
-systemctl daemon-reload
-systemctl status mongod
-systemctl enable mongod
+# # Installing mongodb
+# cd /etc/yum.repos.d/
+# touch mongodb-org-4.0.repo
+# echo "[mongodb-org-4.0]
+# name=MongoDB Repository
+# baseurl=https://repo.mongodb.org/yum/amazon/2013.03/mongodb-org/4.0/x86_64/
+# gpgcheck=1
+# enabled=1
+# gpgkey=https://www.mongodb.org/static/pgp/server-4.0.asc" >> mongodb-org-4.0.repo
+# yum install -y mongodb-org
+# systemctl start mongod
+# systemctl daemon-reload
+# systemctl status mongod
+# systemctl enable mongod
 
 # Installing app dependencies
 cd /home/ec2-user/acebook-t1w3

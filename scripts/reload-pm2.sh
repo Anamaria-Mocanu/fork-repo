57f0baaac4079129d3e59cc7ed2a5280769f23da
ClaimingCookie5 <hotunz@gmail.com>
2022-01-20 10:16:40 +0000

    HE - Echo statement in script

--- scripts/reload-pm2.sh
+++ scripts/reload-pm2.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sudo echo "Changing directory to acebook-t1w3"
-sudo cd /home/ec2-user/acebook-t1w3
+echo "Changing directory to acebook-t1w3"
+cd /home/ec2-user/acebook-t1w3
 sudo npm i pm2@latest -g
 pm2 start npm --name "acebook-t1w3" -- start

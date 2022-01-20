fe34a190cadbd8f33fc1b824af0ca5fd3dc00c44
Anamaria-Mocanu <Annamaariaaa@yahoo.com>
2022-01-20 13:40:31 +0000

    added nvm instalation on pm2.sh

--- scripts/reload-pm2.sh
+++ scripts/reload-pm2.sh
@@ -4,6 +4,7 @@ cd /home/ec2-user/acebook-t1w3
 # nvm bash_completion
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+nvm install 12.14.1
 npm install
 npm i pm2@latest -g
 pm2 start npm --name "acebook-t1w3" -- start

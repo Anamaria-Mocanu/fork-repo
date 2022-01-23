db949da6dba45baaca416fcc281a9cca8532472f
Anamaria Mocanu <80968551+Anamaria-Mocanu@users.noreply.github.com>
2022-01-23 22:26:40 +0000

    commenting out the script
    only left the pm2 start

--- scripts/reload-pm2.sh
+++ scripts/reload-pm2.sh
@@ -5,35 +5,35 @@ touch testfile.txt
 echo "Hello, world" >> testfile.txt
 cat testfile.txt
 
-# Geting the nvm
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
-. ~/.nvm/nvm.sh
-nvm bash_completion
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-nvm install 12.14.1
+# # Geting the nvm
+# curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
+# . ~/.nvm/nvm.sh
+# nvm bash_completion
+# export NVM_DIR="$HOME/.nvm"
+# [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+# nvm install 12.14.1
 
-# Installing mongodb
-cd /etc/yum.repos.d
-touch mongodb-org-4.2.repo
-echo "[mongodb-org-4.2]
-name=MongoDB Repository
-baseurl=https://repo.mongodb.org/yum/amazon/2/mongodb-org/4.2/x86_64/
-gpgcheck=1
-enabled=1
-gpgkey=https://www.mongodb.org/static/pgp/server-4.2.asc" >> mongodb-org-4.2.repo
-yum install -y mongodb-org
-systemctl start mongod
-systemctl daemon-reload
-systemctl status mongod
-systemctl enable mongod
+# # Installing mongodb
+# cd /etc/yum.repos.d
+# touch mongodb-org-4.2.repo
+# echo "[mongodb-org-4.2]
+# name=MongoDB Repository
+# baseurl=https://repo.mongodb.org/yum/amazon/2/mongodb-org/4.2/x86_64/
+# gpgcheck=1
+# enabled=1
+# gpgkey=https://www.mongodb.org/static/pgp/server-4.2.asc" >> mongodb-org-4.2.repo
+# yum install -y mongodb-org
+# systemctl start mongod
+# systemctl daemon-reload
+# systemctl status mongod
+# systemctl enable mongod
 
-# Installing app dependencies
-cd /home/ec2-user/acebook-t1w3
-npm cache clean --force
-rm -rf node_modules package-lock.json
-npm install
-npm i pm2@latest -g
+# # Installing app dependencies
+# cd /home/ec2-user/acebook-t1w3
+# npm cache clean --force
+# rm -rf node_modules package-lock.json
+# npm install
+# npm i pm2@latest -g
 
 # starts application
 pm2 delete all
